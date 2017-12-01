2fd1c75100fb21b7ec5609fad6a36d648ef834ea
Vladimir Zhukov <vzh.inbox@gmail.com>
2017-12-01 17:06:23 +0600

    Remove exports

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// export { default as paths } from './paths';
-// export { default as reactEnv } from './reactEnv';
-import reactEnv from './reactEnv';
-import paths from './paths';
-import * as webpackConfigs from './webpack';
-import * as cssConfigs from './css';
-
-export { reactEnv, paths, webpackConfigs, cssConfigs };
