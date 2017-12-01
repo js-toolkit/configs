b83ecf35c45f37822395095585e340c4bfa1027b
Vladimir Zhukov <vzh.inbox@gmail.com>
2017-12-01 14:29:12 +0600

    Refactor

--- src/ts/index.js
+++
@@ -1,3 +0,0 @@
-export const commonTsLint = require('./common.tslint.json');
-export const reactTsLint = require('./react.tslint.json');
-export const commonTsConfig = require('./common.tsconfig.json');
