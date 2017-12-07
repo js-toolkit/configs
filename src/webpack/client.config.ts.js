9bf8669f762b5ddf43467e7faa0299f27d2bbcc1
Vladimir Zhukov <vzh.inbox@gmail.com>
2017-12-07 16:08:00 +0600

    Fix webpack configs

--- src/webpack/client.config.ts.js
+++ src/webpack/client.config.ts.js
@@ -4,18 +4,18 @@ import paths from '../paths';
 import clientConfig from './client.config';
 import loaders from './loaders';
 
-export const defaultRules = {
-  tsRule: {
-    test: /\.tsx?$/,
-    include: [paths.client.sources, paths.shared.sources],
-    use: loaders.ts({
-      tsconfig: tsconfigPath,
-      forkedChecks: true,
-    }),
-  },
-};
-
 export default ({ entry, rules, tsconfigPath = path.join(paths.client.root, 'tsconfig.json') }) => {
+  const defaultRules = {
+    tsRule: {
+      test: /\.tsx?$/,
+      include: [paths.client.sources, paths.shared.sources],
+      use: loaders.ts({
+        tsconfig: tsconfigPath,
+        forkedChecks: true,
+      }),
+    },
+  };
+
   // Merge and replace rules
   const moduleRules = webpackMerge.strategy(
     Object.getOwnPropertyNames(defaultRules).reduce((obj, name) => ({ ...obj, [name]: 'replace' }))
