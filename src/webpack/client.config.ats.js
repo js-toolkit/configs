0b796e05f79c0a5e68b7d4c9f30ad1d4318881b8
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-01-13 16:06:44 +0600

    Update webpack configs

--- src/webpack/client.config.ats.js
+++ src/webpack/client.config.ats.js
@@ -4,15 +4,15 @@ import paths from '../paths';
 import clientConfig from './client.config';
 import loaders from './loaders';
 
-const defaultRules = {
-  tsRule: {
-    test: /\.tsx?$/,
-    include: [paths.client.sources, paths.shared.sources],
-    use: loaders.ats({ tsconfig: tsconfigPath }),
-  },
-};
-
 export default ({ entry, rules, tsconfigPath = path.join(paths.client.root, 'tsconfig.json') }) => {
+  const defaultRules = {
+    tsRule: {
+      test: /\.tsx?$/,
+      include: [paths.client.sources, paths.shared.sources],
+      use: loaders.ats({ tsconfig: tsconfigPath }),
+    },
+  };
+
   // Merge and replace rules
   const moduleRules = webpackMerge.strategy(
     Object.getOwnPropertyNames(defaultRules).reduce((obj, name) => ({ ...obj, [name]: 'replace' }))
