e6d2b7217be9e02a697c5dc3b1c24588ab24afd4
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-02-28 23:28:21 +0600

    Add webpack universal ats config

---
+++ src/webpack/universal.config.ats.js
@@ -0,0 +1,36 @@
+import webpackMerge from 'webpack-merge';
+import path from 'path';
+import paths from '../paths';
+import reactEnv from '../reactEnv';
+import universalConfig from './universal.config';
+import loaders from './loaders';
+import { defaultRules } from './universal.config.ts';
+
+export { defaultRules };
+
+export default ({ entry, rules, tsconfigPath = path.join(paths.root, 'tsconfig.json') }) => {
+  const { tsRule, ...rest } = defaultRules;
+
+  const useDefaultRules = {
+    tsRule: {
+      ...tsRule,
+      use: loaders.ats({ tsconfig: tsconfigPath }),
+    },
+    ...rest,
+  };
+
+  // Merge and replace rules
+  const moduleRules = webpackMerge.strategy(
+    Object.getOwnPropertyNames(useDefaultRules).reduce(
+      (obj, name) => ({
+        ...obj,
+        [name]: 'replace',
+      }),
+      {}
+    )
+  )(useDefaultRules, rules);
+
+  return webpackMerge(universalConfig({ entry, rules: moduleRules }), {
+    plugins: [loaders.atsCheckerPlugin()],
+  });
+};
