eb4a20c03eaa24a2a2c735c04f61acee2f5cb014
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-07-18 22:34:38 +0600

    Update postcss config

--- src/css/postcssConfig.js
+++ src/css/postcssConfig.js
@@ -1,12 +1,21 @@
 import appEnv from '../appEnv';
+import paths from '../paths';
 
-export default ({ importPath } = {}) => ({
+const defaultImportPath = [paths.client.sources];
+
+export default ({ importPath = defaultImportPath } = {}) => ({
   sourceMap: appEnv.dev,
   plugins: {
-    autoprefixer: appEnv.ifDevMode(false, {}),
     'postcss-import': importPath ? { path: importPath } : false,
     'postcss-icss-values': {},
     'postcss-nested': {},
+    'postcss-preset-env': {
+      stage: 2,
+      features: {
+        'custom-media-queries': true,
+        autoprefixer: appEnv.prod,
+      },
+    },
     cssnano: appEnv.ifDevMode(false, {
       // Need install cssnano cssnano-preset-default
       preset: ['default', { discardComments: { removeAll: true } }],
