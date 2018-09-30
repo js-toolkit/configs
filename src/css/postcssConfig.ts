f382558829b6fab102d6ef2941ab9360d5ba7612
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-10-01 01:16:42 +0600

    Update postcss config

--- src/css/postcssConfig.ts
+++ src/css/postcssConfig.ts
@@ -1,17 +1,17 @@
 import appEnv from '../appEnv';
-import paths from '../paths';
+// import paths from '../paths';
 
 interface Args {
   importPath?: string[] | string;
   presetEnv?: any;
 }
 
-const defaultImportPath = [paths.client.sources];
+// const defaultImportPath = [paths.client.sources];
 
-export default ({ importPath = defaultImportPath, presetEnv }: Args = {}) => ({
+export default ({ /* importPath = defaultImportPath, */ presetEnv }: Args = {}) => ({
   sourceMap: appEnv.dev,
   plugins: {
-    'postcss-import': importPath ? { path: importPath } : false,
+    // 'postcss-import': importPath ? { path: importPath } : false,
     'postcss-icss-values': {},
     'postcss-nested': {},
     'postcss-preset-env': {
@@ -23,9 +23,9 @@ export default ({ importPath = defaultImportPath, presetEnv }: Args = {}) => ({
         ...(presetEnv ? presetEnv.features : undefined),
       },
     },
-    cssnano: appEnv.ifDevMode<any>(false, {
-      // Need install cssnano cssnano-preset-default
-      preset: ['default', { discardComments: { removeAll: true } }],
-    }),
+    // cssnano: appEnv.ifDevMode<any>(false, {
+    //   // Need install cssnano cssnano-preset-default
+    //   preset: ['default', { discardComments: { removeAll: true } }],
+    // }),
   },
 });
