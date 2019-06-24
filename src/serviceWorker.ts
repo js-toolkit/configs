048084c54be3d4d92e1928576ce0912ee7ea9721
Vladimir <vzh.inbox@gmail.com>
2019-06-24 12:36:49 +0600

    Improve serviceWorker

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -27,47 +27,53 @@ const isLocalhost = Boolean(
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
-async function registerValidSW(swUrl: string, config?: Config): Promise<void> {
-  try {
-    const registration = await navigator.serviceWorker.register(swUrl);
-    registration.onupdatefound = () => {
-      const installingWorker = registration.installing;
-      if (installingWorker == null) {
-        return;
-      }
-      installingWorker.onstatechange = () => {
-        if (installingWorker.state === 'installed') {
-          if (navigator.serviceWorker.controller) {
-            // At this point, the updated precached content has been fetched,
-            // but the previous service worker will still serve the older
-            // content until all client tabs are closed.
-            console.log(
-              logPrefix,
-              'New content is available and will be used when all ' +
-                'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-            );
+function registerValidSW(swUrl: string, config?: Config): void {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      // eslint-disable-next-line no-param-reassign
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              // At this point, the updated precached content has been fetched,
+              // but the previous service worker will still serve the older
+              // content until all client tabs are closed.
+              console.log(
+                logPrefix,
+                'New content is available and will be used when all ' +
+                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+              );
 
-            // Execute callback
-            if (config && config.onUpdate) {
-              config.onUpdate(registration);
-            }
-          } else {
-            // At this point, everything has been precached.
-            // It's the perfect time to display a
-            // "Content is cached for offline use." message.
-            console.log(logPrefix, 'Content is cached for offline use.');
+              // Execute callback
+              if (config && config.onUpdate) {
+                config.onUpdate(registration);
+              }
+            } else {
+              // At this point, everything has been precached.
+              // It's the perfect time to display a
+              // "Content is cached for offline use." message.
+              console.log(logPrefix, 'Content is cached for offline use.');
 
-            // Execute callback
-            if (config && config.onSuccess) {
-              config.onSuccess(registration);
+              // Execute callback
+              if (config && config.onSuccess) {
+                config.onSuccess(registration);
+              }
             }
           }
-        }
+        };
+        installingWorker.onerror = error => {
+          console.error(logPrefix, 'Installing worker:', error);
+        };
       };
-    };
-  } catch (error) {
-    console.error(logPrefix, 'Error during service worker registration:', error.message || error);
-  }
+    })
+    .catch(error => {
+      console.error(logPrefix, 'Error during service worker registration:', error.message || error);
+    });
 }
 
 function checkValidServiceWorker(swUrl: string, config?: Config): Promise<void> {
@@ -100,11 +106,11 @@ export function register(config?: Config): void {
   if (appEnv.prod && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(appConfig.client.output.publicPath, window.location.href);
-    // const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      console.warn(logPrefix, 'Origins are different.');
       return;
     }
 
@@ -132,8 +138,10 @@ export function register(config?: Config): void {
   }
 }
 
-export async function unregister(): Promise<void> {
+export function unregister(): void {
   if ('serviceWorker' in navigator) {
-    await navigator.serviceWorker.ready.then(registration => registration.unregister());
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+    });
   }
 }
