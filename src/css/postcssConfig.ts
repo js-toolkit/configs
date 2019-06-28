41a27047c5b9fcf737770725b209e5d4fb9dfd8a
Vladimir <vzh.inbox@gmail.com>
2019-06-28 20:20:39 +0600

    Update postcss config

--- src/css/postcssConfig.ts
+++ src/css/postcssConfig.ts
@@ -4,18 +4,26 @@ import paths from '../paths';
 interface Props {
   importPath?: string[] | string;
   presetEnv?: Record<string, any>;
+  icssValues?: boolean;
+  nested?: boolean;
   minimizer?: boolean;
 }
 
 const defaultImportPath = [paths.client.sources];
 
-export default ({ importPath = defaultImportPath, presetEnv, minimizer = true }: Props = {}) => ({
+export default ({
+  importPath = defaultImportPath,
+  presetEnv,
+  icssValues = true,
+  nested = true,
+  minimizer = true,
+}: Props = {}) => ({
   sourceMap: appEnv.dev,
   plugins: {
     // Primarily use to override imported styles: import css file before css-loader process it and then process merged css by css-loader.
     'postcss-import': importPath ? { path: importPath } : false, // https://github.com/postcss/postcss-import/issues/224
-    'postcss-icss-values': {}, // import values with css-loader not postcss-import
-    'postcss-nested': {},
+    'postcss-icss-values': icssValues ? {} : undefined, // import values with css-loader not postcss-import
+    'postcss-nested': nested ? {} : undefined,
     // https://preset-env.cssdb.org/
     'postcss-preset-env': {
       stage: 2,
