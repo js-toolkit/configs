5cd0b1a0af3ff03881b03b23485005a9c4036cee
Vladimir <vzh.inbox@gmail.com>
2019-08-04 11:19:37 +0600

    Move some code to another package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as appEnv } from './appEnv';
 export { default as appConfig } from './appConfig';
 export { default as paths } from './paths';
-export { default as isStandaloneApp } from './isStandaloneApp';
