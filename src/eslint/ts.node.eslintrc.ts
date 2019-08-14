f394fb5e9f001fdcd40d28683845922f32126ec3
Vladimir <vzh.inbox@gmail.com>
2019-08-14 15:59:13 +0600

    Update ts eslint configs

--- src/eslint/ts.node.eslintrc.ts
+++ src/eslint/ts.node.eslintrc.ts
@@ -1,3 +1,9 @@
+import paths from '../paths';
+
 module.exports = {
   extends: [require.resolve('./node.eslintrc.js'), require.resolve('./ts.common.eslintrc.js')],
+
+  parserOptions: {
+    project: paths.server.tsconfig,
+  },
 };
