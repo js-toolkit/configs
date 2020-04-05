8240a57546eeedef08df3e924538e2f135919850
Vladimir <vzh.inbox@gmail.com>
2020-04-05 15:07:50 +0600

    Fix plugin orders

--- src/babel/react.babelrc.ts
+++ src/babel/react.babelrc.ts
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+    '@babel/plugin-proposal-class-properties',
   ],
 };
