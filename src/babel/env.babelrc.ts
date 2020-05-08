bfa4cc5d81feb98f21ad61c35cdf612a3f61119b
Vladimir <vzh.inbox@gmail.com>
2020-05-08 18:50:11 +0600

    Update babel configs

--- src/babel/env.babelrc.ts
+++ src/babel/env.babelrc.ts
@@ -5,7 +5,7 @@ module.exports = {
   plugins: [
     getInstalledPlugin('@babel/plugin-transform-runtime'),
     getInstalledPlugin('@babel/plugin-proposal-export-namespace-from'),
-    getInstalledPlugin('@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }),
+    getInstalledPlugin('@babel/plugin-proposal-decorators', { legacy: true }),
     getInstalledPlugin('@babel/plugin-proposal-class-properties', { loose: true }),
     getInstalledPlugin('@babel/plugin-proposal-optional-chaining', { loose: true }),
     getInstalledPlugin('@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }),
