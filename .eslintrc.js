05185a45410a27932461138f4998ad5245816cba
Vladimir <vzh.inbox@gmail.com>
2020-06-13 21:48:04 +0600

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,5 +58,10 @@ module.exports = {
       'warn',
       { allowExpressions: true, allowTypedFunctionExpressions: true },
     ],
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
