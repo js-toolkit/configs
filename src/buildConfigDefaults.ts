657a1e14c1dfa07893112ca36fd09d362026272e
Vladimir <vzh.inbox@gmail.com>
2020-08-29 18:24:00 +0600

    Add type

--- src/buildConfigDefaults.ts
+++ src/buildConfigDefaults.ts
@@ -11,7 +11,7 @@ export function getClientConfig(root = 'client') {
       template: '',
       filename: 'index.html',
       title: '',
-    },
+    } as import('html-webpack-plugin').Options,
 
     /** Used by eslint webpack resolver */
     webpackConfig: '',
