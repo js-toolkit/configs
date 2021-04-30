54246e45f7a9118b9b2a79b96802685e680647e4
Vladimir <vzh.inbox@gmail.com>
2021-04-30 10:49:37 +0600

    Update babel config

--- src/babel/env.babelrc.ts
+++ src/babel/env.babelrc.ts
@@ -1,11 +1,13 @@
 import getInstalledPlugin from './getInstalledPlugin';
 
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [['@babel/preset-env', { loose: true }]],
   plugins: [
     getInstalledPlugin('@babel/plugin-transform-runtime'),
     getInstalledPlugin('@babel/plugin-proposal-decorators', { legacy: true }),
-    getInstalledPlugin('@babel/plugin-proposal-class-properties', { loose: true }),
+    getInstalledPlugin('@babel/plugin-proposal-class-properties'),
+    getInstalledPlugin('@babel/plugin-proposal-private-methods'),
+    getInstalledPlugin('@babel/plugin-proposal-private-property-in-object'),
     getInstalledPlugin('@babel/plugin-proposal-export-namespace-from'),
   ].filter((p) => !!p),
 };
