0291db6eaced7a278bafb04721a38248efb0f10c
Vladimir <vzh.inbox@gmail.com>
2023-11-13 09:47:34 +0600

    Use new babel decorators

--- src/babel/env.babelrc.ts
+++ src/babel/env.babelrc.ts
@@ -4,7 +4,7 @@ module.exports = {
   presets: [['@babel/preset-env', { loose: true }]],
   plugins: [
     getInstalledPlugin('@babel/plugin-transform-runtime'),
-    getInstalledPlugin('@babel/plugin-proposal-decorators', { legacy: true }),
+    getInstalledPlugin('@babel/plugin-proposal-decorators', { version: '2023-05' }),
     // Other plugins already included in preset.
   ].filter((p) => !!p),
 };
