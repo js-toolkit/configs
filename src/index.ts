92d9ef2f6dd7ad7a6a9e5cb28ec51e1d07eb29b8
Vladimir <vzh.inbox@gmail.com>
2024-01-09 11:03:17 +0600

    Remove main index file

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as appEnv } from './appEnv';
-export { default as buildConfig } from './buildConfig';
-export { default as apprc } from './apprc'; /** For backward compatibilities. */
-export { default as paths } from './paths';
